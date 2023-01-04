f87fd0b4b865ec6ae1089ee22ea2797630e36255
BrayanGuerreroXD <bg7047653@gmail.com>
2023-01-03 21:44:40 -0500

    Resolve errors

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,22 @@
 import { Weather } from './Weather.js'
-const weather = new Weather('Londo', 'uk')
+import { UI } from './UI.js'
 require('./index.css')
+
+const weather = new Weather('London', 'uk')
+const ui = new UI()
 
 async function fetchWeather() {
     const data = await weather.getWeather()
     console.log(data);
+    ui.render(data)
 }
 
-document.addEventListener('DOMContentLoaded', )
+document.getElementById('w-change-btn').addEventListener('click', () => {
+    const city = document.getElementById('city').value
+    const countryCode = document.getElementById('country-code').value
+    weather.changeLocation(city, countryCode)
+    fetchWeather()
+    e.preventDefault()
+})
+
+document.addEventListener('DOMContentLoaded', fetchWeather)
